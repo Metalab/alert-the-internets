2b7a32f13aa1f081b73e0d344090510ce677b8c8
Moritz Wilhelmy <moritz+git@wzff.de>
2017-03-25 17:55:44 +0100

    hopefully explain things a bit better

--- default_settings.py
+++ default_settings.py
@@ -1,4 +1,10 @@
-SQLITE_DATABASE = "ati.db" # relative to instance directory
 MAX_CONTENT_LENGTH = 256 * 1024 * 1024 # default: 256M max upload to avoid filling the disk
 VIDEO_CONTENT_TYPES = ["video/mp4","video/webm","video/x-matroska"]
+
+# these paths are relative to the instance directory
+SQLITE_DATABASE = "ati.db"
+TITLE_VIDEO = "title.mp4"
+
+# relative to project root directory
 VIDEO_UPLOAD_DIR = "media/original"
+TEMP_DIR = "media/tmp"
